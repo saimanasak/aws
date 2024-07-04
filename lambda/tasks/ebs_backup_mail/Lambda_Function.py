37e950376786d19b185a32cc10b828bfce3fe508
Sai Manasa <saimanasak12@gmail.com>
2024-07-03 19:03:55 -0500

    Update Lambda Function

--- lambda/tasks/ebs_backup_mail/Lambda_Function.py
+++ lambda/tasks/ebs_backup_mail/Lambda_Function.py
@@ -1,8 +1,10 @@
 import boto3
 
-ec2_resource = boto3.resource('ec2')
-sns_client = boto3.client('sns')
+# Initialize AWS clients
+ec2_resource = boto3.resource('ec2') # EC2 resource
+sns_client = boto3.client('sns')      # SNS client
 
+# Define filter to find instances tagged with 'My Server'
 snapshot_filter = [
     {
         'Name': 'tag:My Server',
@@ -10,15 +12,21 @@ snapshot_filter = [
     }
 ]
 
+# An empty list to store the ids of snapshots
 snapshot_details = []
 
+# Iterate through instances matching the filter
 for instance in ec2_resource.instances.filter(Filters=snapshot_filter):
+    # Iterate through volumes attached to the instance
     for volume in instance.volumes.all():
+        # Create a snapshot of the volume with a description
         snapshot = volume.create_snapshot(Description='Created by Lambda')
+        # Append the snapshot ID to snapshot_details list
         snapshot_details.append(snapshot.snapshot_id)
 
+# Publish snapshot details to an SNS topic
 sns_client.publish(
-    TopicArn='arn:aws:sns:us-west-1:068261094692:MyTopic',
+    TopicArn='arn:aws:sns:<AWS-Region>:<AWS-Account-ID>:<SNS-Topic-Name>',
     Subject='My EBS Snapshots',
     Message=str(snapshot_details)
 )
